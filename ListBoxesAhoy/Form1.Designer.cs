e40da365eed00b027db8db042918abb9d0387c73
nstjelja@gmail.com <ns123456>
2014-12-23 21:59:39 +0100

    Added list box

---
+++ ListBoxesAhoy/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace ListBoxesAhoy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstLeft = new System.Windows.Forms.ListBox();
+            this.lstRight = new System.Windows.Forms.ListBox();
+            this.btnMoveOneToRight = new System.Windows.Forms.Button();
+            this.btnMoveAllToRight = new System.Windows.Forms.Button();
+            this.btnMoveAllToLeft = new System.Windows.Forms.Button();
+            this.btnMoveOneToLeft = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstLeft
+            // 
+            this.lstLeft.FormattingEnabled = true;
+            this.lstLeft.Location = new System.Drawing.Point(52, 61);
+            this.lstLeft.Name = "lstLeft";
+            this.lstLeft.Size = new System.Drawing.Size(187, 225);
+            this.lstLeft.TabIndex = 0;
+            // 
+            // lstRight
+            // 
+            this.lstRight.FormattingEnabled = true;
+            this.lstRight.Location = new System.Drawing.Point(351, 61);
+            this.lstRight.Name = "lstRight";
+            this.lstRight.Size = new System.Drawing.Size(219, 225);
+            this.lstRight.TabIndex = 1;
+            // 
+            // btnMoveOneToRight
+            // 
+            this.btnMoveOneToRight.Location = new System.Drawing.Point(259, 61);
+            this.btnMoveOneToRight.Name = "btnMoveOneToRight";
+            this.btnMoveOneToRight.Size = new System.Drawing.Size(75, 23);
+            this.btnMoveOneToRight.TabIndex = 2;
+            this.btnMoveOneToRight.Text = ">";
+            this.btnMoveOneToRight.UseVisualStyleBackColor = true;
+            this.btnMoveOneToRight.Click += new System.EventHandler(this.btnMoveOneToRight_Click);
+            // 
+            // btnMoveAllToRight
+            // 
+            this.btnMoveAllToRight.Location = new System.Drawing.Point(259, 102);
+            this.btnMoveAllToRight.Name = "btnMoveAllToRight";
+            this.btnMoveAllToRight.Size = new System.Drawing.Size(75, 23);
+            this.btnMoveAllToRight.TabIndex = 3;
+            this.btnMoveAllToRight.Text = ">>";
+            this.btnMoveAllToRight.UseVisualStyleBackColor = true;
+            this.btnMoveAllToRight.Click += new System.EventHandler(this.btnMoveAllToRight_Click);
+            // 
+            // btnMoveAllToLeft
+            // 
+            this.btnMoveAllToLeft.Location = new System.Drawing.Point(259, 144);
+            this.btnMoveAllToLeft.Name = "btnMoveAllToLeft";
+            this.btnMoveAllToLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnMoveAllToLeft.TabIndex = 4;
+            this.btnMoveAllToLeft.Text = "<<";
+            this.btnMoveAllToLeft.UseVisualStyleBackColor = true;
+            this.btnMoveAllToLeft.Click += new System.EventHandler(this.btnMoveAllToLeft_Click);
+            // 
+            // btnMoveOneToLeft
+            // 
+            this.btnMoveOneToLeft.Location = new System.Drawing.Point(259, 184);
+            this.btnMoveOneToLeft.Name = "btnMoveOneToLeft";
+            this.btnMoveOneToLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnMoveOneToLeft.TabIndex = 5;
+            this.btnMoveOneToLeft.Text = "<";
+            this.btnMoveOneToLeft.UseVisualStyleBackColor = true;
+            this.btnMoveOneToLeft.Click += new System.EventHandler(this.btnMoveOneToLeft_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 384);
+            this.Controls.Add(this.btnMoveOneToLeft);
+            this.Controls.Add(this.btnMoveAllToLeft);
+            this.Controls.Add(this.btnMoveAllToRight);
+            this.Controls.Add(this.btnMoveOneToRight);
+            this.Controls.Add(this.lstRight);
+            this.Controls.Add(this.lstLeft);
+            this.Name = "Form1";
+            this.Text = "List boy ahoy";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstLeft;
+        private System.Windows.Forms.ListBox lstRight;
+        private System.Windows.Forms.Button btnMoveOneToRight;
+        private System.Windows.Forms.Button btnMoveAllToRight;
+        private System.Windows.Forms.Button btnMoveAllToLeft;
+        private System.Windows.Forms.Button btnMoveOneToLeft;
+    }
+}
+
---
+++ ListBoxesAhoy/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListBoxesAhoy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lstLeft.Items.Add("Sven");
+            lstLeft.Items.Add("Milica");
+            lstLeft.Items.Add("Nikola");
+        }
+
+        private void AfterMove() {
+            if (lstLeft.Items.Count == 0) {
+                btnMoveAllToRight.Enabled = false;
+                btnMoveOneToRight.Enabled = false;
+                btnMoveAllToLeft.Enabled = true;
+                btnMoveOneToLeft.Enabled = true;
+                return;
+            }
+
+            if (lstRight.Items.Count == 0)
+            {
+                btnMoveAllToRight.Enabled = true;
+                btnMoveOneToRight.Enabled = true;
+                btnMoveAllToLeft.Enabled = false;
+                btnMoveOneToLeft.Enabled = false;
+            }
+        }
+
+        private void btnMoveOneToRight_Click(object sender, EventArgs e)
+        {
+            MoveOne(lstLeft, lstRight);
+        }
+
+        private void btnMoveAllToRight_Click(object sender, EventArgs e)
+        {
+            MoveAll(lstLeft, lstRight);
+        }
+
+        private void btnMoveAllToLeft_Click(object sender, EventArgs e)
+        {
+            MoveAll(lstRight, lstLeft);
+        }
+
+        private void btnMoveOneToLeft_Click(object sender, EventArgs e)
+        {
+           
+            MoveOne(lstRight, lstLeft);
+        }
+
+        private void MoveOne(ListBox source, ListBox destination) {
+            if (source.Items.Count == 0)
+                return;
+
+            var item = source.Items[0];
+            source.Items.RemoveAt(0);
+            destination.Items.Add(item);
+            AfterMove();
+        }
+
+        private void MoveAll(ListBox source, ListBox destination) {
+            foreach (var item in source.Items)
+            {
+                destination.Items.Add(item);
+            }
+
+            source.Items.Clear();
+            AfterMove();
+        }
+    }
+}
