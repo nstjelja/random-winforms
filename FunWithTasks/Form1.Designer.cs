3e5aa5be7e098fe6ce21e109d01aecc5bc7a7a39
nstjelja@gmail.com <ns123456>
2014-12-24 22:31:25 +0100

    Sample timer

---
+++ FunWithTasks/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace FunWithTasks
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.time = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // time
+            // 
+            this.time.AutoSize = true;
+            this.time.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.time.Location = new System.Drawing.Point(81, 99);
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(0, 39);
+            this.time.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 262);
+            this.Controls.Add(this.time);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label time;
+    }
+}
+
---
+++ FunWithTasks/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FunWithTasks
+{
+    public partial class Form1 : Form
+    {
+
+        Task runningTask = null;
+        CancellationToken token = new CancellationToken(false);
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            runningTask = Task.Factory.StartNew(() => {
+                while (true) {
+
+                    if (token.IsCancellationRequested)
+                        break;
+
+                    Task.Delay(100).Wait();
+                    var time = DateTime.Now.ToString();
+
+                    Invoke(new Action(() => this.time.Text = time));
+                }
+            }, token,TaskCreationOptions.LongRunning, TaskScheduler.Default);
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (runningTask == null)
+                return;
+
+            token.WaitHandle.Close();
+        }
+
+        
+    }
+}
