0688173e74c35161a2af4499e908782fd37b7778
nstjelja@gmail.com <ns123456>
2014-12-24 22:17:44 +0100

    Sample marshallization

---
+++ FunWithDataSerialization/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithDataSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new DatabaseTable.TestTableAdapters.TestTableTableAdapter();
+            var table = db.GetData();
+
+            var items = table.ToList();
+
+            var serialized = Newtonsoft.Json.JsonConvert.SerializeObject(items);
+            var path = Path.Combine(Environment.CurrentDirectory, "Text.json");
+            File.WriteAllText(path, serialized);
+
+            table.Clear();
+
+            var text =  File.ReadAllText(path);
+
+            var data = Newtonsoft.Json.JsonConvert.DeserializeObject(text);
+        }
+    }
+}
