83ee0daec99b502e866e6708c9258b16d612d855
nstjelja@gmail.com <ns123456>
2014-12-24 23:52:48 +0100

    Completed samples

---
+++ PrintMe/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrintMe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPrintMe_Click(object sender, EventArgs e)
+        {
+            var doc = new System.Drawing.Printing.PrintDocument();
+            doc.PrintPage += doc_PrintPage;
+            doc.Print();
+        }
+
+        void doc_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            e.Graphics.DrawLine(new Pen(Color.Black), new Point(0, 0), new Point(500, 500));
+            e.Graphics.DrawString("My name is Sam", new Font(FontFamily.GenericMonospace, 10),Brushes.Aquamarine, new PointF(300, 300));
+        }
+    }
+}
